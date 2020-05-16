2c9cb73c76bd156bcc0b3d9612070a3af88d04ac
szv <ren_amon@hotmail.com>
2020-05-16 03:00:28 -0400

    Code cleanup for spritegroup

--- mobj/l_spritegroup3d.lua
+++ mobj/l_spritegroup3d.lua
@@ -1,5 +1,5 @@
 --[[
-* l_sprite3dgroup.lua
+* l_spritegroup3d.lua
 * (sprkizard)
 * (May 16, 2020 00:28)
 * Desc: Small script that allows the simple creation of
@@ -14,34 +14,43 @@
 ]]
 
 
-
+-- Builds up a mobj sprite group from a list of mobjs
 local function P_BuildSpriteGroup(source, grouplist)
-	
+
+	-- source mobj is not valid
 	if not source.valid then return end
 
-	source.group_items = grouplist
+	source.sprgroup = grouplist
 end
 
+-- Updates the position and callback functions of the sprite group
 local function P_UpdateSpriteGroup(source, func)
 
+	-- source mobj is not valid
 	if not source.valid then return end
 
-	for i=1,#source.group_items do
+	-- Run through the entire source spritegroup
+	for i=1,#source.sprgroup do
 
-		local groupmobj = source.group_items[i].mobj
-		local offset = source.group_items[i].offset
-		local direction = source.group_items[i].angle
-		local rotation = source.group_items[i].rotation
+		local groupmobj = source.sprgroup[i].mobj
+		local offset = source.sprgroup[i].offset or {x = 0, y = 0, z = 0} -- TODO: be able to exclude each axis; default to 0
+		local direction = source.sprgroup[i].angle or 0
+		local rotation = source.sprgroup[i].rotation or 0
 
+		-- Follow the source angle + independent angle
 		groupmobj.angle = source.angle+FixedAngle(direction*FRACUNIT)
+
+		-- Set scale to source scale
 		groupmobj.scale = source.scale
 
+		-- Run a callback function to edit one or all items
 		if (func) then
-			do func(source.group_items[i])
-
+			do
+				func(source.sprgroup[i])
 			end
 		end
-		-- if not relative then
+
+		-- Update the position of all group items to be relative to the mobj angle + offsets (and scaling!)
 		P_TeleportMove(
 			groupmobj,
 			source.x+FixedMul(offset.x*cos(source.angle+FixedAngle(rotation*FRACUNIT)), groupmobj.scale),
@@ -51,68 +60,6 @@ local function P_UpdateSpriteGroup(source, func)
 
 end
 
-addHook("MobjSpawn", function(mo)
-	P_BuildSpriteGroup(mo, {
-		{mobj = P_SpawnMobj(mo.x, mo.y, mo.z, MT_PULL), offset = {x = 32, y = 32, z = 0}, angle = 90, rotation = 0},
-		{mobj = P_SpawnMobj(mo.x, mo.y, mo.z, MT_PULL), id = "sides", offset = {x = 16, y = 16, z = 0}, angle = 0, rotation = 90},
-		{mobj = P_SpawnMobj(mo.x, mo.y, mo.z, MT_PULL), id = "sides", offset = {x = 16, y = 16, z = 0}, angle = 180, rotation = -90},
-		{mobj = P_SpawnMobj(mo.x, mo.y, mo.z, MT_PULL), offset = {x = 32, y = 32, z = 0}, angle = 270, rotation = 180},
-	})
-end, MT_GR)
-
-addHook("MobjThinker", function(mo)
-
-	P_UpdateSpriteGroup(mo, function(item)
-		if item.id then
-			item.mobj.frame = 1
-		end
-		item.mobj.sprite = SPR_BOXS
-		item.mobj.frame = $1|FF_PAPERSPRITE
-		item.mobj.scale = max(FRACUNIT/4, cos(ANG1*leveltime)*4)
-	end)
-
-	mo.angle = $1+ANG1
-	if (leveltime % 90 == 0) then 
-		mo.momz = 8*FRACUNIT
-	end
-	--P_DrawFacingMarker(mo, SKINCOLOR_RED)
-
-end, MT_GR)
-
-
-addHook("MobjSpawn", function(mo)
-	P_BuildSpriteGroup(mo, {
-		{mobj = P_SpawnMobj(mo.x, mo.y, mo.z, MT_PULL), offset = {x = 0, y = 0, z = 0}, angle = 0, rotation = 0},
-		{mobj = P_SpawnMobj(mo.x, mo.y, mo.z, MT_PULL), offset = {x = 0, y = 0, z = 0}, angle = 110, rotation = 0},
-		{mobj = P_SpawnMobj(mo.x, mo.y, mo.z, MT_PULL), offset = {x = 0, y = 0, z = 0}, angle = 230, rotation = 0},
-		{mobj = P_SpawnMobj(mo.x, mo.y, mo.z, MT_PULL), id = "top", offset = {x = 0, y = 0, z = 64}, angle = 0, rotation = 0},
-	})
-end, MT_GOLDENPEDESTAL)
-
-addHook("MobjThinker", function(mo)
-
-	P_UpdateSpriteGroup(mo, function(item)
-		if item.id == "top" then
-			item.mobj.sprite = SPR_RING
-		else
-			item.mobj.sprite = SPR_PEDG
-			item.mobj.frame = $1|FF_PAPERSPRITE
-		end
-	end)
-
-	--P_DrawFacingMarker(mo, SKINCOLOR_RED)
-
-end, MT_GOLDENPEDESTAL)
-
-
-
-addHook("ThinkFrame", function()
-
-	for player in players.iterate do
-		if leveltime == 1*TICRATE then
-			-- local group = P_SpawnMobj(player.mo.x+64*FRACUNIT, player.mo.y, player.mo.floorz, MT_GR)
-			local group = P_SpawnMobj(player.mo.x+64*FRACUNIT, player.mo.y, player.mo.floorz, MT_GOLDENPEDESTAL)
-			--group.angle = ANGLE_90
-		end
-	end
-end)
+-- Expose globals
+rawset(_G, "P_BuildSpriteGroup", P_BuildSpriteGroup)
+rawset(_G, "P_UpdateSpriteGroup", P_UpdateSpriteGroup)
