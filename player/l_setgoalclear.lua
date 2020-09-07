87042b6e96b230a1703d3d9c078a41ec92b524c1
szv <sblossom49@gmail.com>
2020-09-07 18:47:58 -0400

    Update l_setgoalclear.lua

--- player/l_setgoalclear.lua
+++ player/l_setgoalclear.lua
@@ -8,14 +8,14 @@ rawset(_G, "P_SetGoalClear", function (source, spawnorigin, playerMobj)
     if not (special.DummyGoal and special.DummyGoal.valid) then
         special.DummyGoal = P_SpawnMobj(special.x, special.y, special.z, MT_SIGN)
         special.DummyGoal.spawnpoint = spawnorigin
-		special.DummyGoal.state = S_SIGNSPIN1
-		special.DummyGoal.momz = 8*FRACUNIT
-		
-		S_StartSound(thing, special.DummyGoal.info.seesound)
-		
-		special.DummyGoal.flags2 = $1|MF2_DONTDRAW
+        special.DummyGoal.state = S_SIGNSPIN1
+        special.DummyGoal.momz = 8*FRACUNIT
+        
+        S_StartSound(thing, special.DummyGoal.info.seesound)
+        
+        special.DummyGoal.flags2 = $1|MF2_DONTDRAW
         special.DummyGoal.tracer.flags2 = $1|MF2_DONTDRAW
-		special.DummyGoal.tracer.tracer.flags2 = $1|MF2_DONTDRAW
+        special.DummyGoal.tracer.tracer.flags2 = $1|MF2_DONTDRAW
     end
 
     -- Do the magic clearing stuff here now
