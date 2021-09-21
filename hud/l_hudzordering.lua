47e4341f6fba11ff38ba0360bb05e0a058da2bae
szv <sblossom49@gmail.com>
2021-09-20 22:11:14 -0400

    Update multi-hud (
    Adds custom argument variable declaration into the Hud function, and makes the scripting a little less reliant on raw table editing for setup
    
    +(Add netvars function)

--- hud/l_hudzordering.lua
+++ hud/l_hudzordering.lua
@@ -23,6 +23,9 @@
 -- spairs attribution by:
 -- https://stackoverflow.com/questions/15706270/sort-a-table-in-lua
 -------------------
+-- Do not add this twice if it is in another file, please
+if _G["hudLayer"] then return end
+
 
 -- Attempts to sort pairs
 local function spairs(t, order)
@@ -49,90 +52,71 @@ local function spairs(t, order)
 end
 
 
-
 -- The container table that holds all user created HUD elements
-rawset(_G, "hudLayer", {})
-
--- Some examples
---[[
-hudLayer["test1"] = {
-	layernum = 5,
-	func = (function(v, stplyr, cam)
-		v.drawString(120, 64, "Layer 5", V_ALLOWLOWERCASE, "left")
-	end)
-}
-
-hudLayer["test2"] = {
-	layernum = 4,
-	func = (function(v, stplyr, cam) 
-		v.drawString(126, 66, "Layer 4", V_ALLOWLOWERCASE, "left")
-	end)
-}
-
-hudLayer["test3"] = {
-	layernum = 9,
-	func = (function(v, stplyr, cam) 
-		v.drawString(129, 67, "Layer 9", V_ALLOWLOWERCASE, "left")
-	end)
-}
-
-hudLayer["removable"] = {
-	layernum = 3,
-	func = (function(v, stplyr, cam) 
-		v.drawString(129, 67, "Remove me on Jump", V_ALLOWLOWERCASE, "left")
-	end)
-}
---]]
-
+rawset(_G, "hudLayer", {items={}})
 
 
 -- Iterates and sorts through hudLayer with spairs
 local function sortHudItems(v, stplyr, cam)
 
-	--for k,huditem in spairs(hudLayer, function(t,a,b) return t[b].layernum < t[a].layernum end) do -- desc
-	for _,huditem in spairs(hudLayer, function(t,a,b) return t[a].layernum > t[b].layernum end) do -- asc
+	--for k,huditem in spairs(hudLayer.items, function(t,a,b) return t[b].layernum < t[a].layernum end) do -- desc
+	for _,huditem in spairs(hudLayer.items, function(t,a,b) return t[a].layernum > t[b].layernum end) do -- asc
 		
-		-- Delete the item
+		-- Delete the item (Stop it from running at all and skip it)
 		if (huditem.layernum == -99) then
-			huditem = nil
+			-- huditem = nil
 			return
 		end
 
 		(function()
-			-- Do not run on indexes with -1 at all.
+			-- Do not run on items with -1 at all.
 			if (huditem.layernum == -1) then return end
 			
 			-- Run hud functions
-			huditem.func(v, stplyr, cam)
+			huditem.func(huditem.args, v, stplyr, cam)
 		end)()
 	end
 end
 hud.add(sortHudItems, "game")
 
--- Adds a new layer
-local function R_AddHud(layername, ordernum, hudfunc)
-	hudLayer[layername] = {
-		layernum = ordernum,
-		func = (function(v, stplyr, cam) hudfunc(v, stplyr, cam) end)
+-- Adds a new hud to the layer table
+local function R_AddHud(layername, ordernum, args, hudfunc, forcereset)
+	hudLayer.items[layername] = {
+		args = args or {},
+		-- reset = setreset or true, (Unused)
+		layernum = ordernum or -1,
+		func = hudfunc,
 	}
 end
 
 -- Sets layer order
-local function R_SetHudOrder(layername, ordernum)
-	if not (hudLayer and hudLayer[layername]) then return end
-	hudLayer[layername].layernum = ordernum
+local function R_SetHud(layername, ordernum, args)
+	if not (hudLayer and hudLayer.items[layername]) then return end
+
+	-- TODO: find another way to keep the default value
+	hudLayer.items[layername].layernum = (ordernum and type(ordernum) == "string" and $ or ordernum or -1) 
+	-- hudLayer.items[layername].layernum = ordernum or -1
+	
+	-- Add or update variables to the Hud if specified
+	if (args) then
+		for k,v in pairs(args) do
+			(function()
+				hudLayer.items[layername].args[k] = v
+			end)()
+		end
+	end
 end
 
 -- Disables a layer
 local function R_DisableHud(layername)
-	if not (hudLayer and hudLayer[layername]) then return end
-	hudLayer[layername].layernum = -1
+	if not (hudLayer and hudLayer.items[layername]) then return end
+	hudLayer.items[layername].layernum = -1
 end
 
 -- Sets the layer to be deleted
 local function R_DeleteHud(layername)
-	if not (hudLayer and hudLayer[layername]) then return end
-	hudLayer[layername].layernum = -99
+	if not (hudLayer and hudLayer.items[layername]) then return end
+	hudLayer.items[layername].layernum = -99
 end
 
 -- Sets internal game huds to be hidden/unhidden
@@ -146,26 +130,52 @@ local function R_SetInternalHudStatus(namelist, huditemvisible)
 	end
 end
 
+-- Netvars hook / function
+function hudLayer.netvars(n)
+	for _,entry in pairs(hudLayer.items) do
+		entry.args = n($)
+		entry.layernum = n($)
+		-- entry.func = n($)
+	end
+end
+
+--[[addHook("NetVars", function(network)
+	hudLayer.netvars(network)
+end)--]]
+
 rawset(_G, "R_AddHud", R_AddHud)
-rawset(_G, "R_SetHudOrder", R_SetHudOrder)
+rawset(_G, "R_SetHud", R_SetHud)
 rawset(_G, "R_DisableHud", R_DisableHud)
 rawset(_G, "R_DeleteHud", R_DeleteHud)
 rawset(_G, "R_SetInternalHudStatus", R_SetInternalHudStatus)
 
 
+-- Some examples
 -- (Uncomment to test)
--- Test the addon by removing removable layer, and replacing the text on 'test1'
---[[
+-- Test the script addon by removing the removable layer, and replacing the text on 'test1'
+--[[R_AddHud("test1", 5, {text=nil}, -- Custom text variable
+function(args, v, stplyr)
+	v.drawString(64, 64, args.text or "Jump to replace my text!", V_ALLOWLOWERCASE, "left")
+end)
+
+R_AddHud("test2", 4, {},
+function(args, v, stplyr)
+	local x = 6*cos(leveltime*ANG10)/FU
+	v.drawString(64+x, 68, "Moving text!", V_ALLOWLOWERCASE, "left")
+end)
+
+R_AddHud("removable", 3, {},
+function(args, v, stplyr)
+	v.drawString(64, 76, "Remove me on Jump", V_ALLOWLOWERCASE, "left")
+end)
+
 addHook("ThinkFrame", function()
 
 	for player in players.iterate do
-		if (player.cmd.buttons & BT_USE) then
-		
-			hudLayer["removable"].layernum = -1
-			hudLayer["test1"].func = (function(v, stplyr, cam)
-				v.drawString(120, 64, "Replaced Text!", V_ALLOWLOWERCASE, "left")
-			end)
+		if (player.cmd.buttons & BT_JUMP) then
+			R_DeleteHud("removable")
+			R_SetHud("test1", "", {text="Replaced Text!"})
 		end
 	end
-end)
---]]
+end)--]]
+
