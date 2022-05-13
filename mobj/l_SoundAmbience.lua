2b9e8bb3bd9fcea74127256b8a16d00892eb0061
szv <sblossom49@gmail.com>
2022-05-12 22:27:12 -0400

    Update l_SoundAmbience.lua
    finalizes sound looping script concept from 2020;
    - replaces global soundtable with spawnable mobj
    - adds looping current soundid in list with second argument
    - attach the soundplayer to a mobj or valid position

--- mobj/l_SoundAmbience.lua
+++ mobj/l_SoundAmbience.lua
@@ -1,186 +1,103 @@
 
--- create mobj sound source
+-- reimplementation of soundtable.lua (2020)
+-- TODO: format file
 
-freeslot("MT_SOUNDPLAYER", "S_SOUNDPLAYER_SPAWN", "S_SOUNDPLAYER_SET", "S_SOUNDPLAYER_PLAY", "S_SOUNDPLAYER_END")
+
+-- note: only for loops, use S_StartSound for one entry that has no looptime
+
+freeslot("MT_SOUNDPLAYER", "S_SOUNDPLAYER_SET", "S_SOUNDPLAYER_PLAY", "S_SOUNDPLAYER_END")
 
 mobjinfo[MT_SOUNDPLAYER] = {
     doomednum = -1,
-    spawnstate = S_SOUNDPLAYER_SPAWN,
-    -- seesound = sfx_laser,
-    -- speed = 8*FRACUNIT,
-    -- radius = 41*FRACUNIT,
-    -- height = 41*FRACUNIT,
-    -- damage = 2,
-    -- mass = 100,
-    flags = MF_NOGRAVITY
+    spawnstate = S_SOUNDPLAYER_SET,
+    flags = MF_NOGRAVITY|MF_NOCLIPHEIGHT|MF_NOCLIP,
 }
-states[S_SOUNDPLAYER_SPAWN] = {SPR_THOK,0|FF_TRANS40,1,nil,0,0,S_SOUNDPLAYER_SET}
-states[S_SOUNDPLAYER_SET] = {SPR_THOK,0|FF_TRANS40,1,function(mo)
+states[S_SOUNDPLAYER_SET] = {SPR_NULL,0|FF_TRANS40,1,function(mo)
+    -- set loop
     mo.audiosrc.loop = mo.audiosrc.sfxlist[mo.audiosrc.num][2]
-    -- print("test")
 end,0,0,S_SOUNDPLAYER_PLAY}
-states[S_SOUNDPLAYER_PLAY] = {SPR_THOK,0|FF_TRANS40,1,function(mo)
+states[S_SOUNDPLAYER_PLAY] = {SPR_NULL,0|FF_TRANS40,1,function(mo)
 
     local soundid = mo.audiosrc.sfxlist[mo.audiosrc.num]
+    local loopnum = mo.audiosrc.sfxlist[mo.audiosrc.num][2]
 
-    -- if (mo.audiosrc.num > #mo.audiosrc.sfxlist) then
-    --     mo.state = S_SOUNDPLAYER_END
-    --     print("ended")
-    --     return
-    -- else
-    -- if (mo.audiosrc.num < #mo.audiosrc.sfxlist) then
-        
-        if S_SoundPlaying(mo, soundid[1]) then
+    if S_SoundPlaying(mo, soundid[1]) then
 
-            -- stop loop while playing    
-            if mo.audiosrc.loop and mo.audiosrc.loop == 1 then
-                print("stopped")
-                S_StopSound(mo, mo.audiosrc.sfxlist[mo.audiosrc.num][1])
-                mo.audiosrc.loop = $1 - 1
-                return
-            end
-        else
-            
-            if (mo.audiosrc.loop > 0) then
-                S_StartSound(mo, mo.audiosrc.sfxlist[mo.audiosrc.num][1])
-                -- mo.state = S_SOUNDPLAYER_PLAY
-                return
-            end
-            -- -- repeat starts over, once ends after last sound,
-            -- -- none or any string repeats last sound endlessly
-            -- if isLoop == "repeat" then
-            --     mo.i = 1
-            -- elseif isLoop == "once" then 
-            --     return 
-            -- else
-            --     mo.i = #mo.audiosrc.sfxlist 
-            -- end
-            -- print(debugmsg)
-
-            --[[if not mo.audiosrc.playing then
-                mo.audiosrc.playing = true
-            else
-                mo.audiosrc.num = $1 + 1
-            end--]]
-            -- and mo.audiosrc.sfxlist[mo.audiosrc.num][2]
-            if (mo.audiosrc.num < #mo.audiosrc.sfxlist) then
-                mo.audiosrc.num = (mo.audiosrc.playing) and $1+1 or $1+0
-                mo.audiosrc.playing = true
-
-                 -- play sound
-                print(string.format("soundid %s", tostring(mo.audiosrc.num)))
-                S_StartSound(mo, mo.audiosrc.sfxlist[mo.audiosrc.num][1])
-                mo.state = S_SOUNDPLAYER_SET
-            else
-                
-        mo.state = S_SOUNDPLAYER_END
-            end
+        -- stop loop while playing    
+        if mo.audiosrc.loop and mo.audiosrc.loop == 1 then
+            S_StopSound(mo, mo.audiosrc.sfxlist[mo.audiosrc.num][1])
+            mo.audiosrc.loop = $1 - 1
+            -- print("stopped loop")
+            return
         end
-    -- else
-    --     mo.state = S_SOUNDPLAYER_END
-    --     print("ended")
-    --     -- mo.audiosrc.playing = true
-    -- end
-    print(string.format("loop - %d", tostring(mo.audiosrc.loop)))
-    mo.audiosrc.loop = max(0, $1-1)
-    -- print(string.format("loop - %d", tostring(soundid[2])))
-    -- soundid[2] = max(0, $1-1)
-
-   --[[ if not S_SoundPlaying(mo, soundid[1]) then
+    else
+        -- TODO: looppoint with third argument
+        -- TODO: re-add repeat mode
+        -- TODO: infinite loop
+        --[[ if (mo.audiosrc.loop == -1) then
+            S_StartSound(mo, mo.audiosrc.sfxlist[mo.audiosrc.num][1])
+            return
+        end--]]
+        -- reset sound in loop
+        if (mo.audiosrc.loop > 0) then
+            S_StartSound(mo, mo.audiosrc.sfxlist[mo.audiosrc.num][1])
+            return
+        end
         
-        if mo.audiosrc.num <= #mo.audiosrc.sfxlist then
-            -- if not mo.audiosrc.playing then
-            --     mo.audiosrc.playing = true
-            -- else
-            --     mo.audiosrc.num = $1 + 1
-            -- end
+        if (mo.audiosrc.num < #mo.audiosrc.sfxlist) then
             mo.audiosrc.num = (mo.audiosrc.playing) and $1+1 or $1+0
             mo.audiosrc.playing = true
+
+            -- play next sound and reset state
+            -- print(string.format("soundid %s", tostring(mo.audiosrc.num)))
+            S_StartSound(mo, mo.audiosrc.sfxlist[mo.audiosrc.num][1])
+            mo.state = S_SOUNDPLAYER_SET
         else
-            -- works but repeats final sound twice
-            if (soundid[2]) then
-                -- mo.state = S_SOUNDPLAYER_SET
-            else
-                mo.state = S_SOUNDPLAYER_END
-                print("ended")
-            end
-           
-            -- -- repeat starts over, once ends after last sound,
-            -- -- none or any string repeats last sound endlessly
-            -- if isLoop == "repeat" then
-            --     mo.i = 1
-            -- elseif isLoop == "once" then 
-            --     return 
-            -- else
-            --     mo.i = #mo.audiosrc.sfxlist 
-            -- end
-            -- print(debugmsg)
+            -- hit end of list
+            mo.state = S_SOUNDPLAYER_END
+            -- print("Ended")
+            return
         end
-         -- play sound
-        print(mo.audiosrc.num)
-        S_StartSound(mo, mo.audiosrc.sfxlist[mo.audiosrc.num][1])
-    else
-        -- mo.audiosrc.playing = true
     end
-    soundid[2] = max(0, $1-1)--]]
-   
+
+    -- print(string.format("loop - %d", tostring(mo.audiosrc.loop)))
+    mo.audiosrc.loop = max(0, $1-1)
+
 end,0,0,S_SOUNDPLAYER_PLAY}
-states[S_SOUNDPLAYER_END] = {SPR_THOK,0|FF_TRANS40,5*TICRATE,nil,0,0,S_NULL}
+states[S_SOUNDPLAYER_END] = {SPR_THOK,0|FF_TRANS40,1,nil,0,0,S_NULL}
 
 
-local soundsourcelist = {}
 
-local function P_CreateSoundSource(sounds, soundorigin, args)
+
+-- Creates a mobj sound source
+local function S_StartSoundEnviro(sounds, soundorigin, args)
 
     local src = P_SpawnMobj(soundorigin.x, soundorigin.y, soundorigin.z, MT_SOUNDPLAYER)
-    -- src.tics = INT8_MAX
-    -- src.color = SKINCOLOR_WHITE
 
-    src.audiosrc = {soundmobj=src, sfxlist=sounds, playing=false, num=1, loop=0, origin=soundorigin, static=true, length=1*TICRATE}
-    -- table.insert(soundsourcelist, {soundmobj=src, sfxlist=sounds, listnum=1, origin=soundorigin, static=true, length=1*TICRATE})
+    src.audiosrc = {soundmobj=src, sfxlist=sounds, playing=false, num=1, loop=0, origin=soundorigin, static=(args and args.static)}
+
+    -- TODO: reference created object properly
+    return src
 end
+rawset(_G, "S_StartSoundEnviro", S_StartSoundEnviro)
 
--- local function S_PlayAmbience(isAMobjSource, source, soundnum, player)
---     if (isAMobjSource) then
-
---         if not S_SoundPlaying(source, soundnum) then
---             S_StartSound(source, soundnum, player)
---         end
---     else
---         if not S_IdPlaying(soundnum) then
---             S_StartSound(source, soundnum, player)
---         end
---     end
--- end
--- rawset(_G, "S_PlayAmbience", S_PlayAmbience)
 
 addHook("MobjSpawn", function(mo)
     mo.audiosrc = {}
-
 end)
 
+addHook("MobjThinker", function(mo)
+
+    if not mo and mo.audiosrc then return end
+
+    if (mo.audiosrc.origin and mo.audiosrc.origin.valid) and not mo.audiosrc.static then
+        P_TeleportMove(mo, mo.audiosrc.origin.x, mo.audiosrc.origin.y, mo.audiosrc.origin.z)
+    end
+end)
 
 
 addHook("ThinkFrame", function()
     if leveltime == 2*TICRATE then
-        -- P_CreateSoundSource({sfx_eleva2}, server.mo, {static=true})
-        P_CreateSoundSource({{sfx_jump,0}, {sfx_eleva1,0}, {sfx_eleva2, 8*TICRATE}, {sfx_eleva3,0}}, server.mo, {static=true})
-        -- P_CreateSoundSource({{sfx_jump,0}, {sfx_eleva1,0}, {sfx_eleva2, 5*TICRATE}}, server.mo, {static=true})
+        S_StartSoundEnviro({{sfx_jump,0}, {sfx_eleva1,0}, {sfx_eleva2, 8*TICRATE}, {sfx_eleva3,0}}, server.mo, {static=false})
     end
 end)
-
---[[addHook("ThinkFrame", function()
-    for i=1,#soundsourcelist do
-        local source = soundsourcelist[i]
-        if not source.soundmobj.valid then continue end
-
-        
-        if not S_SoundPlaying(source.soundmobj, source.sfxlist[source.listnum]) then
-            S_StartSound(source.soundmobj, source.sfxlist[source.listnum])
-        else
-            source.listnum = $1+1
-        end
-
-        P_TeleportMove(source.soundmobj, source.origin.x, source.origin.y, source.origin.z)
-    end
-end)--]]
